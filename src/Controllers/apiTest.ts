6018afc911fb626e241e6fc00e3e567f3f4db4cf
Nour Alhadi Mahmoud <nour.m@socienta.com>
2021-03-19 14:13:54 +0200

    Fixed an issue in test controller

--- src/Controllers/apiTest.ts
+++ src/Controllers/apiTest.ts
@@ -1,3 +1,4 @@
+import { logInfo } from "./../Config/logging";
 import { Response, Request } from "express";
 import { StatusCodes } from "http-status-codes";
 import { config } from "../Config/config";
@@ -5,5 +6,6 @@ import { config } from "../Config/config";
 const API_URL = config.server.hostname + ":" + config.server.port + "/api/test";
 
 export const test = async ( req: Request, res: Response ): Promise<void> => {
-    res.status( StatusCodes.OK ).send( API_URL + req.url );
+    logInfo( "TEST API", "Request Recieved!");
+    res.status( StatusCodes.OK ).send( { data: ("" + API_URL + req.url) } );
 };
